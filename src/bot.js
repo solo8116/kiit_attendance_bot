02ae00d2360848daab236727a5f84dd7bc3b570b
solo8116 <150136452+solo8116@users.noreply.github.com>
2024-04-05 22:49:41 +0530

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -16,11 +16,15 @@ const bot = async (user, pass) => {
     const page = await browser.newPage();
     await page.goto(url);
 
+    console.log("went to url");
+
     let username = await page.waitForSelector(
       `xpath///input[@id="logonuidfield"]`
     );
 
     await username.type(userId);
+
+    console.log("typed username");
 
     let password = await page.waitForSelector(
       `xpath///input[@id="logonpassfield"]`
@@ -28,17 +32,25 @@ const bot = async (user, pass) => {
 
     await password.type(userPassword);
 
+    console.log("typed password");
+
     let submit = await page.waitForSelector(
       `xpath///input[@class="urBtnStdNew"]`
     );
 
     await submit.click();
 
+    console.log("clicked submit");
+
     let selectAboveBtn = await page.waitForSelector(
       `xpath///td[@onclick="doMouseClick(1,1);return false;"]`
     );
 
     await selectAboveBtn.click();
+
+    console.log("above button clicked");
+
+    
     await new Promise((resolve, reject) => setTimeout(resolve, 1000));
     var iframeHandler = await page.$('iframe[name="Desktop Inner Page    "]');
     const serviceFrame = await iframeHandler.contentFrame();
@@ -48,11 +60,15 @@ const bot = async (user, pass) => {
     );
     await studentSelfServiceLink.click();
 
+    console.log("student self service clicked");
+
     const selectStudentAttendanceDetailsLink =
       await serviceFrame.waitForSelector(
         `xpath///span[@class="urTxtStd" and @ct="TV" and contains(text(), "Student Attendance Details")]`
       );
     await selectStudentAttendanceDetailsLink.click();
+
+    console.log("student attendance details clicked");
 
     await new Promise((resolve, reject) => setTimeout(resolve, 1000));
 
@@ -67,6 +83,8 @@ const bot = async (user, pass) => {
 
     await yearInput.click();
 
+    console.log("year input");
+
     let year = await attendanceFrame.waitForSelector(
       `xpath///div[@data-itemvalue1="2023-2024"]`
     );
@@ -79,6 +97,8 @@ const bot = async (user, pass) => {
 
     await sessionInput.click();
 
+    console.log("session clicked");
+    
     let session = await attendanceFrame.waitForSelector(
       `xpath///div[@data-itemvalue1="Spring"]`
     );
@@ -100,6 +120,8 @@ const bot = async (user, pass) => {
     let attendanceSubmit = await attendanceFrame.waitForSelector(
       `xpath///div[@lsdata="{0:'Submit',2:'EMPHASIZED'}"]`
     );
+
+    console.log("submit btn clicked");
 
     await attendanceSubmit.click();
     await new Promise((resolve, reject) => setTimeout(resolve, 2000));
